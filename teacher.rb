cfb306c75ed051f297abc654fc0d1041be7b2156
Emmanuel Mhango <emmanuel.mhango@bsu.edu.az>
2023-01-31 01:02:22 +0400

    Initialization

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require './person.rb'
+
+class Teacher < Person
+
+  def initialize(specialization)
+    super()
+    @specialization= specialization
+  end
+
+  def can_use_services?
+    true
+  end
+
+end
