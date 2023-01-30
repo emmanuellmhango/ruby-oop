ef9eb076abe4c909c32f762764bcd92e2e4f87e0
Emmanuel Mhango <emmanuel.mhango@bsu.edu.az>
2023-01-31 01:55:57 +0400

    Clear linter erors

--- person.rb
+++ person.rb
@@ -2,14 +2,12 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initilize(age, name = 'Unknown', parent_permission: true)
-    @id = nil
+  def initilize(age, name = 'Unknown', parent_permission = true)
+    @id = rand(1_000_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-
-  private
 
   def can_use_services?
     return true if is_of_age? || @parent_permission
@@ -17,7 +15,8 @@ class Person
     false
   end
 
-  def of_age?
+  private
+  def is_of_age?
     @age > 18
   end
 end
