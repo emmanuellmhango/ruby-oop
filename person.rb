482864e54ac34a8efb492a528304b720174d16d0
Emmanuel Mhango <emmanuel.mhango@bsu.edu.az>
2023-02-02 06:43:34 +0400

    Clear linter error

--- person.rb
+++ person.rb
@@ -7,7 +7,6 @@ class Person < Nameable
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    super()
     @id = rand(1_000_000)
     @name = name
     @age = age
