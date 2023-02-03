387f1856c89b51dbddd541f0a862f38569169c93
Emmanuel Mhango <emmanuel.mhango@bsu.edu.az>
2023-02-04 01:47:16 +0400

    Clear errors

--- main.rb
+++ main.rb
@@ -1,6 +1,10 @@
 require_relative 'app'
 
 class Main
+  def initialize
+    @app = App.new
+  end
+
   def options
     puts 'Please choose an option by entering a number'
     puts '1 - List all books'
@@ -13,8 +17,59 @@ class Main
     print 'Option: '
   end
 
+  def register_people
+    print 'Do you want to create a student (1) or a Teacher (2)? [Input the number]:'
+    selection = gets.chomp
+    num = selection.to_i
+    case num
+    when 1
+      print 'Age: '
+      age = gets.chomp.to_i
+      print 'Name: '
+      name = gets.chomp
+      print 'Has parent permission? [Y/N]: '
+      parent_permission = gets.chomp.downcase == 'y'
+      print 'Classroom: '
+      classroom = gets.chomp
+      @app.create_student(classroom, age, name, parent_permission)
+    when 2
+      print 'Age: '
+      age = gets.chomp.to_i
+      print 'Name: '
+      name = gets.chomp
+      print 'Specialization: '
+      specialization = gets.chomp
+      @app.create_teacher(age, name, specialization)
+    else
+      puts 'Wrong Input.'
+    end
+  end
+
+  def rent_book
+    puts 'Select a book from the following list by number'
+    @app.list_books_for_rental
+    print 'Book number: '
+    book_number = gets.chomp.to_i
+    puts
+    puts 'Select a person from the following list by number (not ID)'
+    @app.list_people_to_rent_a_book
+    print 'Person number: '
+    person_number = gets.chomp.to_i
+    puts
+    print 'Date: '
+    date = gets.chomp
+    @app.rent_a_book(date, person_number, book_number)
+  end
+
+  def register_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    @app.create_book(title, author)
+  end
+
   def menu
-    app = App.new
     exit_code = true
     while exit_code
       options
@@ -22,59 +77,19 @@ class Main
       choice = input.to_i
       case choice
       when 1
-        app.list_books
+        @app.list_books
       when 2
-        app.list_people
+        @app.list_people
       when 3
-        print 'Do you want to create a student (1) or a Teacher (2)? [Input the number]:'
-        selection = gets.chomp
-        num = selection.to_i
-        case num
-        when 1
-          print 'Age: '
-          age = gets.chomp.to_i
-          print 'Name: '
-          name = gets.chomp
-          print 'Has parent permission? [Y/N]: '
-          parent_permission = gets.chomp.downcase == 'y'
-          print 'Classroom: '
-          classroom = gets.chomp
-          app.create_student(classroom, age, name, parent_permission)
-        when 2
-          print 'Age: '
-          age = gets.chomp.to_i
-          print 'Name: '
-          name = gets.chomp
-          print 'Specialization: '
-          specialization = gets.chomp
-          app.create_teacher(age, name, specialization)
-        else
-          puts 'Wrong Input.'
-        end
+        register_people
       when 4
-        print 'Title: '
-        title = gets.chomp
-        print 'Author: '
-        author = gets.chomp
-        app.create_book(title, author)
+        register_book
       when 5
-        puts 'Select a book from the following list by number'
-        app.list_books_for_rental
-        print 'Book number: '
-        book_number = gets.chomp.to_i
-        puts
-        puts 'Select a person from the following list by number (not ID)'
-        app.list_people_to_rent_a_book
-        print 'Person number: '
-        person_number = gets.chomp.to_i
-        puts
-        print 'Date: '
-        date = gets.chomp
-        app.rent_a_book(date, person_number, book_number)
+        rent_book
       when 6
         print 'ID of person: '
         id = gets.chomp.to_i
-        app.list_rentals(id)
+        @app.list_rentals(id)
       when 7
         exit_code = false
         puts
