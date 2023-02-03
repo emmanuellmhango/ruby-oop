ce4ec4d423691350b53f28883e0a40c269a04f87
Emmanuel Mhango <emmanuel.mhango@bsu.edu.az>
2023-02-04 01:13:56 +0400

    ADD UI

--- app.rb
+++ app.rb
@@ -14,14 +14,14 @@ class App
     puts
     book = Book.new(title, author)
     @books.push(book)
-    puts "Book created successfully"
+    puts 'Book created successfully'
     puts
   end
 
   def list_books
     puts
     if @books.length < 1
-      puts "No books added yet"
+      puts 'No books added yet'
     else
       @books.each {|book| puts "Title: \"#{book.title}\", Author: #{book.author}"}
     end
@@ -31,7 +31,7 @@ class App
   def list_people
     puts
     if @people.length < 1
-      puts "No people added yet"
+      puts 'No people added yet'
     else
       @people.each {|person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"}
     end
@@ -42,7 +42,7 @@ class App
     student = Student.new(classroom, age, name: name, parent_permission: parent_permission)
     puts
     @people.push(student)
-    puts "Person created successfully"
+    puts 'Person created successfully'
     puts
   end
   
@@ -50,14 +50,14 @@ class App
     teacher = Teacher.new(age, specialization, name: name)
     @people.push(teacher)
     puts
-    puts "Person created successfully"
+    puts 'Person created successfully'
     puts
   end
 
   def list_books_for_rental
     puts
     if @books.length < 1
-      puts "No books added"
+      puts 'No books added'
     else
       @books.each_with_index {|book, index| puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"}
     end
@@ -67,7 +67,7 @@ class App
   def list_people_to_rent_a_book
     puts
     if @people.length < 1
-      puts "No people added"
+      puts 'No people added'
     else
       @people.each_with_index {|person, index| puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"}
     end
@@ -78,7 +78,7 @@ class App
     puts
     rental = Rental.new(date, @people[person], @books[book])
     @rentals.push(rental)
-    puts "Rental created successfully"
+    puts 'Rental created successfully'
     puts
   end
 
--- main.rb
+++ main.rb
@@ -1,23 +1,23 @@
 require_relative 'app'
 
 def options
-  puts "Please choose an option by entering a number"
-  puts "1 - List all books"
-  puts "2 - List all people"
-  puts "3 - Create a person"
-  puts "4 - Create a book"
-  puts "5 - Create a rental"
-  puts "6 - List all rentals for a given person id"
-  puts "7 - Exit"
-  print "Option: "
+  puts 'Please choose an option by entering a number'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+  print 'Option: '
 end
 
 def main
   app = App.new
   exit_code = true
   while exit_code
-    options()
-    input = gets.chomp()
+    options
+    input = gets.chomp
     choice = input.to_i
     case choice
     when 1
@@ -25,49 +25,49 @@ def main
     when 2
       app.list_people
     when 3
-      print "Do you want to create a student (1) or a Teacher (2)? [Input the number]:"
-      selection = gets.chomp()
+      print 'Do you want to create a student (1) or a Teacher (2)? [Input the number]:'
+      selection = gets.chomp
       num = selection.to_i
       case num
       when 1
-        print "Age: "
+        print 'Age: '
         age = gets.chomp.to_i
-        print "Name: "
+        print 'Name: '
         name = gets.chomp
-        print "Has parent permission? [Y/N]: "
+        print 'Has parent permission? [Y/N]: '
         parent_permission = gets.chomp.downcase == 'y'
-        print "Classroom: "
+        print 'Classroom: '
         classroom = gets.chomp
         app.create_student(classroom, age, name, parent_permission)
       when 2
-        print "Age: "
+        print 'Age: '
         age = gets.chomp.to_i
-        print "Name: "
+        print 'Name: '
         name = gets.chomp
-        print "Specialization: "
+        print 'Specialization: '
         specialization = gets.chomp
         app.create_teacher(age, name, specialization)
       else
-        puts "Wrong Input."
+        puts 'Wrong Input.'
       end
     when 4
-      print "Title: "
+      print 'Title: '
       title = gets.chomp
-      print "Author: "
+      print 'Author: '
       author = gets.chomp
       app.create_book(title, author)
     when 5
-      puts "Select a book from the following list by number"
+      puts 'Select a book from the following list by number'
       app.list_books_for_rental
-      print "Book number: "
+      print 'Book number: '
       book_number = gets.chomp.to_i
       puts
-      puts "Select a person from the following list by number (not ID)"
+      puts 'Select a person from the following list by number (not ID)'
       app.list_people_to_rent_a_book
-      print "Person number: "
+      print 'Person number: '
       person_number = gets.chomp.to_i
       puts
-      print "Date: "
+      print 'Date: '
       date = gets.chomp
       app.rent_a_book(date, person_number, book_number)
     when 6
@@ -76,15 +76,13 @@ def main
       app.list_rentals(id)
     when 7
       exit_code = false
-      puts " "
-      puts "See you next time!!"
-      puts " "
+      puts
+      puts 'See you next time!!'
+      puts
     else
-      puts "You chose a wrong option, use options 1 to 7"
+      puts 'You chose a wrong option, use options 1 to 7'
     end 
   end
 end
 
-main()
-
-  
+main
